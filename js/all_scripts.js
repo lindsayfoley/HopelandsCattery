1b456c727be4be9d6a25982ab3e5189f6474a605
Lindsay Prahalad <lprahalad@hotmail.co.uk>
2015-04-19 19:47:46 +0100

    Updates to pages and script
    completed about us.
    Updated directions with bus route.
    Added new images to about us.
    Changed script to detect current page.
    Minified CSS

--- js/all_scripts.js
+++ js/all_scripts.js
@@ -1,49 +1,34 @@
-
 var CurrentPageIdentifier = {
 	
-	run: function(pageNavId) {
-		CurrentPageIdentifier.removeLinkOfCurrentPage(pageNavId);
+	run: function() {
+		CurrentPageIdentifier.removeLinkOfCurrentPage();
 	},
 	
-	removeLinkOfCurrentPage: function(pageNavId) {
-		var prefix = '#';
-		var currentPage = prefix.concat(pageNavId);
-		var uppercaseNavName = pageNavId.toUpperCase();
-		$(currentPage).replaceWith(uppercaseNavName);
-	},
-	
-	highlightCurrentPage: function() {
+	pageUrl: window.location.pathname.substring(1),
+	removeLinkOfCurrentPage: function() {
+
+		$('nav ul li a').each(function() {
 		
+			var link = $(this).attr('href');
+			var pageNameToReplaceLinkName = $(this).text();
+			
+			if(CurrentPageIdentifier.pageUrl === link) {
+				$(this).parent('li').attr('id', 'currentPage').text(pageNameToReplaceLinkName);
+				$(this).remove();
+			} 
+		});
+		
+		if(CurrentPageIdentifier.pageUrl.length < 1) {
+			$('nav ul li:first-child').attr('id', 'currentPage').text('HOME');
+			$(this).find('a').remove();
+		}
 	}
 };
 
-var catteryLatLong = [51.911675, -2.12611];
- 
-function showGoogleMaps() {
- 
-var latLng = new google.maps.LatLng(catteryLatLong[0], catteryLatLong[1]);
- 
-var mapOptions = {
-	zoom: 15, // initialize zoom level - the max value is 21
-	streetViewControl: false, // hide the yellow Street View pegman
-	scaleControl: true, // allow users to zoom the Google Map
-	mapTypeId: google.maps.MapTypeId.ROADMAP,
-	center: latLng
-};
- 
-map = new google.maps.Map(document.getElementById('maps_background'),mapOptions);
- 
-// Show the default red marker at the location
-var iconBase = 'http://hopelandscattery.com';
-marker = new google.maps.Marker({
-	position: latLng,
-	map: map,
-	draggable: false,
-	animation: google.maps.Animation.DROP,
-	icon: iconBase + '/imagesX/logo_grey.png'
-	});
-}
-google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+$(document).ready(function() {
+	CurrentPageIdentifier.run();
+});
+
 
 
 
